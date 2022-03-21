493667174899ac30cb08597b8bbd500cc391d0ea
Filip Chovanec <61948151+GandelXIV@users.noreply.github.com>
2022-03-21 08:58:48 +0100

    Update math.c
    Removed disfunctional pow() from math.c

--- lib/math.c
+++ lib/math.c
@@ -1,18 +1,9 @@
 #include "math.h"
 
-double pow(double base, double exponent)  // only positive numbers
-{
-  double result = 1;
-  while (exponent > 0)
-  {
-    result *= base;
-    --exponent;
-  }
-  return result;
-}
 
 double abs(double number)
 {
   if (number < 0) return -number;
   return number;
 }
+
